662b44c80695a8c600b76b2c67666a40b13b9a08
xspyhack <xspyhack@gmail.com>
2014-05-29 20:35:19 +0800

    add user info models

---
+++ weibo/models.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+'''
+
+class UserInfo():
+    nickname = None
+    location = None
+    sex = None
+    birth = None
+    regday = None
+    email = None
+    qq = None
+    edu = []
+    career = []
+    tags = []
+    fans = 0
+    follows = 0
+    weibo = []
+
